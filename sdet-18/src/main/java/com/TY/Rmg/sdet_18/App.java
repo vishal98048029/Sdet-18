8ba7eaaf9dc04f57dfc50b2f9aede18d16417fc3
vishal <meravishal1992@rediffmail.com>
2021-08-03 17:15:48 +0530

    this is my 1st commit

---
+++ sdet-18/src/main/java/com/TY/Rmg/sdet_18/App.java
@@ -0,0 +1,13 @@
+package com.TY.Rmg.sdet_18;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ sdet-18/src/test/java/com/TY/Rmg/sdet_18/Maven1.java
@@ -0,0 +1,12 @@
+package com.TY.Rmg.sdet_18;
+
+import org.testng.annotations.Test;
+
+public class Maven1 {
+	@Test
+	public void maven1()
+	{
+		System.out.println("this is user1 and its my 1st commit");
+	}
+
+}
